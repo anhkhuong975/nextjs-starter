e6bff0770a4ed0a3e282aff152bb1e865aa79e81
khuong <khuong>
2020-12-19 22:12:24 +0700

    update apt get time

--- pages/api/get-time.ts
+++ pages/api/get-time.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (_: NextApiRequest, res: NextApiResponse) => {
-    const datetime = new Date();
-    res.status(200).json({ api: 'get-time', datetime });
+    const offset = 7;
+    const datetime = new Date( new Date().getTime() + offset * 3600 * 1000)
+        .toUTCString().replace( / GMT$/, "" )
+    res.status(200).json({ api: 'get-time', datetime, offset});
 }
